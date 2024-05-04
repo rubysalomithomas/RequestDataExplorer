7f7fa65050a5fc030015102c22a8a1b917e90125
rubysalomithomas <157551580+rubysalomithomas@users.noreply.github.com>
2024-05-04 15:11:47 -0500

    Update RequestDataExplorer.py

--- RequestDataExplorer.py
+++ RequestDataExplorer.py
@@ -13,7 +13,7 @@ st.sidebar.header('User Input Features')
 # Web scraping of Request data
 @st.cache(allow_output_mutation=True)
 def load_data():
-    all_tx_url = "https://api.etherscan.io/api?module=account&action=txlistinternal&address=0x7b3c4d90e8af6030d66c07f8f815f9505e379d6f&startblock=0&endblock=999999999&sort=desc&apikey=A37KV8A3HWW2PSUG3K2EF6BCJVNWCPKDP8"
+    all_tx_url = "https://jsonplaceholder.typicode.com/todos/1"
     json = pd.read_json(all_tx_url)
    
 
