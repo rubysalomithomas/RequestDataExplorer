8e37d380a797634015e8bdb814ecff13dc64b4d9
rubysalomithomas <157551580+rubysalomithomas@users.noreply.github.com>
2024-05-07 17:09:10 -0500

    Update RequestDataExplorer.py

--- RequestDataExplorer.py
+++ RequestDataExplorer.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 st.title('Test simple API')
 
-option = st.sidebar.selectbox(
+year = st.sidebar.selectbox(
     "Year",
     ("2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019","2020", "2021", "2022", "2023"))
 
@@ -15,14 +15,14 @@ st.write("You selected:", option)
 # Web scraping of Request data
 # https://api.census.gov/data/2021/cps/basic/jan?get=PELKAVL,PELKDUR,PELKFTO,PELKLL1O,PELKLL2O,PELKLWO,PELKM1&for=state:51&PEEDUCA=39
 @st.cache(allow_output_mutation=True)
-def load_data(option):
-    all_tx_url = "https://jsonplaceholder.typicode.com/todos/" + option
+def load_data(year):
+    all_tx_url = "https://jsonplaceholder.typicode.com/todos/" + year
     st.write(all_tx_url)
-    st.write(option)
+    st.write(year)
     json =  requests.get(all_tx_url)
     return json
 
 
-reqdata = load_data(option)
+reqdata = load_data(year)
 
 st.json(reqdata.text)
