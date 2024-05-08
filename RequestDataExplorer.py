41e1c4f503af6aeb566590ba0006f0a22df5c649
rubysalomithomas <157551580+rubysalomithomas@users.noreply.github.com>
2024-05-07 19:35:58 -0500

    Update RequestDataExplorer.py

--- RequestDataExplorer.py
+++ RequestDataExplorer.py
@@ -15,10 +15,20 @@ month = st.sidebar.selectbox(
     ("jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct","nov", "dec"))
 state = st.sidebar.selectbox(
     "State",
-    ("1", "2", "3", "4", "5", "6", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "7", "7", "7", "7", "7", "7", "7", "7", "7"))
+    ("1", "2", "3", "4", "5", "6", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21"))
 variable = st.sidebar.selectbox(
     "Variables",
     ("PELKAVL", "PELKDUR", "PELKFTO", "PELKLL1O", "PLKLL2O", "PELKLWO", "PELKM1"))
+CHOICES = {1: "dataset a", 2: "dataset b", 3: "dataset c"}
+
+
+def format_func(option):
+    return CHOICES[option]
+
+
+option = st.selectbox("Select option", options=list(CHOICES.keys()), format_func=format_func)
+st.write(f"You selected option {option} called {format_func(option)}")
+
 st.write("You selected:", year,month,variable)
 # Web scraping of Request data
 # https://api.census.gov/data/2021/cps/basic/jan?get=PELKAVL,PELKDUR,PELKFTO,PELKLL1O,PELKLL2O,PELKLWO,PELKM1&for=state:51&PEEDUCA=39
