5a7ad78d924f4eba2539aa4b9f664d50ca5e0e2f
rubysalomithomas <157551580+rubysalomithomas@users.noreply.github.com>
2024-05-07 21:21:05 -0500

    Update RequestDataExplorer.py

--- RequestDataExplorer.py
+++ RequestDataExplorer.py
@@ -50,6 +50,5 @@ def load_data(year,month,state,variable):
 response = load_data(year,month,state,variable)
 
 dataframe = pd.DataFrame(response) 
-ind_list = [0]
 
-st.dataframe(dataframe.iloc[ind_list])
+st.dataframe(list(dataframe.index.values))
