3b1e60ed3a79d9ba73119538e83a69634a9d7b5d
rubysalomithomas <157551580+rubysalomithomas@users.noreply.github.com>
2024-05-13 10:37:31 -0500

    Update RequestDataExplorer.py

--- RequestDataExplorer.py
+++ RequestDataExplorer.py
@@ -61,7 +61,7 @@ if response.status_code == 200:
  df = pd.DataFrame(data[1:], columns=data)
 
 # Display the DataFrame
- st.write(df[df['PELKAVL'] > -1])
+ st.write(df.columns)
 else:
  st.write("Failed to retrieve data:", response.status_code)
 
