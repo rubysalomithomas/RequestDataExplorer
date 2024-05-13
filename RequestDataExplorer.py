fe36995edb70f53ba61158546f8cd003ba89f9fe
rubysalomithomas <157551580+rubysalomithomas@users.noreply.github.com>
2024-05-13 18:55:46 -0500

    Update RequestDataExplorer.py

--- RequestDataExplorer.py
+++ RequestDataExplorer.py
@@ -45,6 +45,7 @@ if uploaded_file is not None:
         st.write(f"### Unemployed looking-search methods", grouped_data)
     elif variable =="PELKAVL":
         job_search_methods = {"1": "Yes", "2": "No"}
+        df = pd.DataFrame(data)
         df2 = df[['PELKAVL']]
         df2["PELKAVL"] = df2["PELKAVL"].map(job_search_methods)
         test = pd.DataFrame(df2["PELKAVL"])
