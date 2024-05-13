c9e362d041e098d04bec7ea69735976665e99558
rubysalomithomas <157551580+rubysalomithomas@users.noreply.github.com>
2024-05-13 09:31:44 -0500

    Update RequestDataExplorer.py

--- RequestDataExplorer.py
+++ RequestDataExplorer.py
@@ -49,6 +49,6 @@ def load_data(year,month,state,variable):
 
 response = load_data(year,month,state,variable)
 
-dataframe = pd.DataFrame(response, index=[0, 1])
+dataframe = pd.DataFrame(response)
 #dd= dataframe.sum()
 st.write(dataframe)
