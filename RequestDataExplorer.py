5488d06c5b9e77117c5d07742589302dddb85f66
rubysalomithomas <157551580+rubysalomithomas@users.noreply.github.com>
2024-05-13 13:33:56 -0500

    Update RequestDataExplorer.py

--- RequestDataExplorer.py
+++ RequestDataExplorer.py
@@ -81,7 +81,7 @@ if response.status_code == 200 and response.text:
                 group_column = st.sidebar.selectbox('Select column to group by:', df.columns)
                 
                 # Perform grouping
-                grouped_data = df['PELKM1'].groupby(group_column).sum()
+                grouped_data = df.groupby(group_column,'state').sum()
                 
                 # Create a plot using Plotly
                 #fig = px.bar(grouped_data, x=group_column, y='PELKM1', title='Total Values by Category')
